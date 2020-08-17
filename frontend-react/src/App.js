d6a50d3cf880891946e5c808a5d30ca1af83442e
ak3l <pierre.bourdet1@gmail.com>
2020-08-17 02:16:48 +0200

    React - Installed env-cmd

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,27 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    fetch('http://192.168.10.10/api')
+    fetch(process.env.REACT_APP_API_URL + '/api')
         .then(response => response.json())
         .then(data => console.log(data.test));
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
