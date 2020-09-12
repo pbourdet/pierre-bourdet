d80f937595a48810c220852902eb3430b7c13e5e
ak3l <pierre.bourdet1@gmail.com>
2020-09-12 18:28:28 +0200

    Added put and delete todo security

---
+++ symfony/src/Events/Todo/TodoModificationSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events\Todo;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Authorization\UserAuthorizationChecker;
+use App\Entity\Todo;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class TodoModificationSubscriber implements EventSubscriberInterface
+{
+    private const METHODS_ALLOWED = [
+        Request::METHOD_PUT,
+        Request::METHOD_DELETE,
+    ];
+
+    private UserAuthorizationChecker $userAuthorizationChecker;
+
+    public function __construct(UserAuthorizationChecker $userAuthorizationChecker)
+    {
+        $this->userAuthorizationChecker = $userAuthorizationChecker;
+    }
+
+    /**
+     * @return array<array>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ['check', EventPriorities::PRE_VALIDATE],
+        ];
+    }
+
+    public function check(ViewEvent $event): void
+    {
+        /** @var Todo $todo */
+        $todo = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($todo instanceof Todo && in_array($method, self::METHODS_ALLOWED)) {
+            /** @var User $user */
+            $user = $todo->getUser();
+            $this->userAuthorizationChecker->check($user);
+        }
+    }
+}
