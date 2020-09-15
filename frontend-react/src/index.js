b6ecc4c690bf89eeb7296fbe40f0a1dab913f497
ak3l <pierre.bourdet1@gmail.com>
2020-09-16 01:45:41 +0200

    Installed sentry for react

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    environment: process.env.REACT_APP_ENV,
+    dsn: "https://cc5180a8227641cfac0e43488011ce66@o437767.ingest.sentry.io/5401259",
+    integrations: [
+        new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <React.StrictMode>
