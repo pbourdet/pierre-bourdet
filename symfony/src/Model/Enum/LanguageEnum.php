a0ef1fe0a9a11737f561ee4a6e5749e0b7d67a87
pierre b <pierre.bourdet1@gmail.com>
2021-05-30 00:37:14 +0200

    Create LanguageEnum

---
+++ symfony/src/Model/Enum/LanguageEnum.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Enum;
+
+final class LanguageEnum
+{
+    public const EN_ISO639 = 'en-GB';
+    public const FR_ISO639 = 'fr-FR';
+
+    public const EN = 'en';
+    public const FR = 'fr';
+
+    public const LANGUAGES_MAPPER = [
+        self::EN_ISO639 => self::EN,
+        self::FR_ISO639 => self::FR,
+    ];
+
+    public static function getIsoLanguages(): array
+    {
+        return [
+            self::EN_ISO639,
+            self::FR_ISO639,
+        ];
+    }
+
+    public static function resolveLanguage(string $isoLanguage): string
+    {
+        return self::LANGUAGES_MAPPER[$isoLanguage];
+    }
+}
