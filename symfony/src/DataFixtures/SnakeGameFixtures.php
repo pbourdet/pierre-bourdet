fe87ff62f026e9231f05e4a70f5533d844be9285
pierre b <pierre.bourdet1@gmail.com>
2021-06-29 22:14:59 +0200

    Create fixtures

---
+++ symfony/src/DataFixtures/SnakeGameFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\SnakeGame;
+use App\Repository\UserRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Uid\Uuid;
+
+class SnakeGameFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function __construct(
+        private UserRepository $userRepository
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $users = $this->userRepository->findAll();
+
+        $faker = Factory::create();
+
+        foreach ($users as $user) {
+            for ($g = 0; $g < 5; ++$g) {
+                $snakeGame = new SnakeGame();
+
+                $snakeGame
+                    ->setId(Uuid::v4())
+                    ->setScore($faker->numberBetween(int2: 1000))
+                    ->setUser($user);
+
+                $manager->persist($snakeGame);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+}
