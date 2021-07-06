e83499d77e0c77040e62416465bd4a51c6b116aa
pierre b <pierre.bourdet1@gmail.com>
2021-07-07 01:09:46 +0200

    Changed SportRadar client with private request method

--- symfony/src/Domain/Tennis/SportRadarClient.php
+++ symfony/src/Domain/Tennis/SportRadarClient.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Domain\Tennis;
 
+use Model\Tennis\Exception\SportRadarApiException;
+use Model\Tennis\ExternalModel\PlayerProfile\PlayerProfile;
 use Model\Tennis\ExternalModel\Rankings\RankingsBaseClass;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -13,20 +16,48 @@ class SportRadarClient
 {
     public function __construct(
         private HttpClientInterface $client,
-        private SerializerInterface $serializer
+        private SerializerInterface $serializer,
+        private LoggerInterface $logger
     ) {
     }
 
     public function getSinglesRankings(): RankingsBaseClass
     {
+        return $this->request(
+            '/tennis/trial/v3/fr/rankings.json',
+            RankingsBaseClass::class
+        );
+    }
+
+    public function getPlayerProfile(string $playerId): object
+    {
+        return $this->request(
+            sprintf('/tennis/trial/v3/en/competitors/%s/profile.json', $playerId),
+            PlayerProfile::class
+        );
+    }
+
+    private function request(string $url, string $outputClass): mixed
+    {
         $response = $this->client->request(
             Request::METHOD_GET,
-            '/tennis/trial/v3/fr/rankings.json',
+            $url
         );
+
+        if ($response->getStatusCode() >= 400) {
+            $this->logger->error(
+                sprintf('Error when calling URL %s with status code %s', $url, $response->getStatusCode()),
+                [
+                    'message' => $response->getContent(false),
+                ]
+            );
+
+            throw new SportRadarApiException($response->getStatusCode(), $response->getContent(false), );
+        }
 
         return $this->serializer->deserialize(
             $response->getContent(),
-            RankingsBaseClass::class,
+            $outputClass,
             'json'
         );
     }
---
+++ symfony/src/Model/Tennis/Exception/SportRadarApiException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Tennis\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SportRadarApiException extends HttpException
+{
+}
