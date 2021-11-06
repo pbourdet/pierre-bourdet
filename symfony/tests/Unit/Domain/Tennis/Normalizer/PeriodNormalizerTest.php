6888d8cbf394978660dd7c40708268e5017a7532
pierre b <pierre.bourdet1@gmail.com>
2021-11-07 00:04:15 +0100

    Ran php cs fixer

--- symfony/tests/Unit/Domain/Tennis/Normalizer/PeriodNormalizerTest.php
+++ symfony/tests/Unit/Domain/Tennis/Normalizer/PeriodNormalizerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit\Domain\Tennis\Normalizer;
 
 use Domain\Tennis\Normalizer\PeriodNormalizer;
--- symfony/tests/Unit/Infrastructure/Serializer/NameConverter/CamelCaseNameConverterTest.php
+++ symfony/tests/Unit/Infrastructure/Serializer/NameConverter/CamelCaseNameConverterTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit\Infrastructure\Serializer\NameConverter;
 
 use Infrastructure\Serializer\NameConverter\CamelCaseNameConverter;
