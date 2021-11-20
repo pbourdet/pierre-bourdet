d84fa4a389b6ad0aecd3e183d22f11171b1dca91
pierre b <pierre.bourdet1@gmail.com>
2021-11-20 22:12:59 +0100

    Add mercure event source  for real time updates

--- frontend-react/src/components/Messaging/ConversationTabs/index.js
+++ frontend-react/src/components/Messaging/ConversationTabs/index.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Tab, Col, Nav, Row } from 'react-bootstrap';
 import { useAuth, useAuthUpdate } from '../../../contexts/AuthContext';
 import PropTypes from 'prop-types';
-import { getConversations } from '../../../requests/messaging';
+import { getConversations, createMercureEventSource } from '../../../requests/messaging';
 import ConversationTab from '../ConversationTab';
 import Loader from '../../Loader';
 import ConversationDisplay from '../ConversationDisplay';
 import CreateConversationModal from '../CreateConversationModal';
 import CopyIdButton from '../CopyIdButton';
 import { FormattedMessage } from 'react-intl';
+import { toast } from 'react-toastify';
 
 function ConversationTabs () {
     const user = JSON.parse(localStorage.getItem('user'));
@@ -19,6 +20,21 @@ function ConversationTabs () {
     const [showMessages, setShowMessages] = useState(false);
     const [isMobile] = useState(Number(window.innerWidth) < 768);
     const [activeKey, setActiveKey] = useState('');
+    const [eventSource, setEventSource] = useState(null);
+
+    useEffect(() => {
+        const newEventSource = eventSource ?? createMercureEventSource(process.env.REACT_APP_API_URL + '/conversations');
+
+        newEventSource.onmessage = function (event) {
+            const conversation = JSON.parse(event.data);
+            const otherParticipant = conversation.participants.find(participant => participant.user.id !== user.id);
+
+            setConversations([conversation, ...conversations]);
+            toast.info(<FormattedMessage id="messaging.create.conversation.other" values={{ name: otherParticipant.user.nickname }}/>);
+        };
+
+        setEventSource(newEventSource);
+    }, [conversations]);
 
     useEffect(() => {
         (async () => {
@@ -58,7 +74,7 @@ function ConversationTabs () {
 
     return (
         <>
-            <Tab.Container defaultActiveKey={conversations[0].id} activeKey={activeKey} onSelect={(k) => setActiveKey(k)} mountOnEnter>
+            <Tab.Container activeKey={activeKey || conversations[0].id} onSelect={(k) => setActiveKey(k)} mountOnEnter>
                 <Row className="m-0 h-100">
                     <Col style={{ overflowY: 'overlay' }} className={`p-0 h-100 border-right ${(!isMobile || (isMobile && !showMessages)) ? '' : 'd-none'}`} md={4}>
                         <div style={{ backgroundColor: 'azure' }} className="border-bottom pt-2 pb-2">
